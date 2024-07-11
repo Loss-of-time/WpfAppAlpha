88796c24d52d95a13379fcd645c7b59fb22710d1
Loss <1399651432@qq.com>
2024-07-11 18:43:22 +0800

    修复manager 中的tag混淆问题

--- ManagerWinodow.xaml.cs
+++ ManagerWinodow.xaml.cs
@@ -22,13 +22,13 @@ namespace WpfAppAlpha
             var majors = _context.Major.ToList();
             foreach (var major in majors)
             {
-                var majorNode = new TreeViewItem { Header = major.MajorName, Tag = major.MajorNo };
+                var majorNode = new TreeViewItem { Header = major.MajorName, Tag = "Major_" + major.MajorNo };
                 TreeViewData.Items.Add(majorNode);
 
                 var students = _context.Student.Where(s => s.MajorNo == major.MajorNo).ToList();
                 foreach (var student in students)
                 {
-                    var studentNode = new TreeViewItem { Header = student.Sname, Tag = student.Sno };
+                    var studentNode = new TreeViewItem { Header = student.Sname, Tag = "Student_" + student.Sno };
                     majorNode.Items.Add(studentNode);
                 }
             }
@@ -39,22 +39,31 @@ namespace WpfAppAlpha
             var selectedItem = TreeViewData.SelectedItem as TreeViewItem;
             if (selectedItem == null) return;
 
-            if (selectedItem.Tag is int majorNo)
+            var tag = selectedItem.Tag as string;
+            if (tag == null) return;
+
+            if (tag.StartsWith("Major_"))
             {
-                var students = _context.Student.Where(s => s.MajorNo == majorNo).ToList();
-                DataGridData.ItemsSource = students;
+                if (int.TryParse(tag.Substring(6), out int majorNo))
+                {
+                    var students = _context.Student.Where(s => s.MajorNo == majorNo).ToList();
+                    DataGridData.ItemsSource = students;
+                }
             }
-            else if (selectedItem.Tag is int sno)
+            else if (tag.StartsWith("Student_"))
             {
-                var courses = _context.CourseSelect
-                                      .Where(cs => cs.Sno == sno)
-                                      .Select(cs => cs.Cno)
-                                      .Join(_context.Course,
-                                            cno => cno,
-                                            course => course.Cno,
-                                            (cno, course) => course)
-                                      .ToList();
-                DataGridData.ItemsSource = courses;
+                if (int.TryParse(tag.Substring(8), out int sno))
+                {
+                    var courses = _context.CourseSelect
+                                          .Where(cs => cs.Sno == sno)
+                                          .Select(cs => cs.Cno)
+                                          .Join(_context.Course,
+                                                cno => cno,
+                                                course => course.Cno,
+                                                (cno, course) => course)
+                                          .ToList();
+                    DataGridData.ItemsSource = courses;
+                }
             }
         }
     }
